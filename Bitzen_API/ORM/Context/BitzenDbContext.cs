e7e491c8b075a04d8e817fd0e61f6aba81d67a72
gabriel <gabrielosantosb@gmail.com>
2025-04-02 20:12:17 -0300

    InitialMigrations

---
+++ Bitzen_API/ORM/Context/BitzenDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Bitzen_API.ORM.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bitzen_API.ORM.Context
+{
+    public class BitzenDbContext : DbContext
+    {
+        public BitzenDbContext(DbContextOptions<BitzenDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserModel> User { get; set; }
+
+    }
+}
--- Bitzen_API/Program.cs
+++ Bitzen_API/Program.cs
@@ -1,3 +1,14 @@
+using Bitzen_API.Application.Services.Token;
+using Bitzen_API.Application.Services.User;
+using Bitzen_API.ORM.Context;
+using Bitzen_API.ORM.Entity;
+using Bitzen_API.ORM.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +16,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region postgresconfig
+builder.Services.AddDbContext<BitzenDbContext>(options =>
+       options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+#endregion postgresconfig
+
+
+builder.Services.AddAuthorization();
+
+
 
 var app = builder.Build();
 
