4194e069604b7e67021f1e59f1348ef087539ba4
gabriel <gabrielosantosb@gmail.com>
2025-04-02 19:23:08 -0300

    first commit, ajusting baseRepository

--- Bitzen_API/ORM/Repository/BaseRepository.cs
+++ Bitzen_API/ORM/Repository/BaseRepository.cs
@@ -5,7 +5,34 @@ namespace Bitzen_API.ORM.Repository
     public class BaseRepository<T> : IBaseRepository<T> where T : class
 
     {
+        public T Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
 
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
 
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
